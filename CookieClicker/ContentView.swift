6a9f3ea7e68a60d5b8f1f8980da693813563928d
Alex Geibel <alex.geibel@icloud.com>
2024-04-17 11:10:40 -0400

    Initial Commit

---
+++ CookieClicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookieClicker
+//
+//  Created by Alexander Geibel on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookieClicker/CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieClickerApp.swift
+//  CookieClicker
+//
+//  Created by Alexander Geibel on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
